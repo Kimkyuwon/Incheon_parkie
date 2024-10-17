cc520fa40b5b9e671dce111cb403001eec306cef
heejeonglim <heejeong.lim@hlcompany.com>
2024-10-17 13:52:22 +0900

    [24.10.17][lhj] wheel_detectio인국공출장수정사항

--- make_engine/src/pt_onnx.py
+++ make_engine/src/pt_onnx.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("../../enter_car_ws/weights/ver8n.pt") 
 
 # Export the model 
-model.export(format='engine',imgsz=(480,640),batch=2,int8= True,simplify=True, dynamic=False,device=0)
+model.export(format='onnx',imgsz=(480,640),batch=2,int8= True,simplify=True, dynamic=False,device=0)
 
 # make engine process
 # step1 : python3 pt_onnx_.py -> onnx file generated , engine file creation failed due to unknown issue
